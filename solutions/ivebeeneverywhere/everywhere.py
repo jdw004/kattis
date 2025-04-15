24dee6706181faef7a54c4ca90023d53be6524c4
John Welch <132951168+jdw004@users.noreply.github.com>
2025-04-14 21:17:59 -0500

    Delete solutions/ivebeeneverywhere directory

--- solutions/ivebeeneverywhere/everywhere.py
+++
@@ -1,9 +0,0 @@
-n = int(input())
-
-for _ in range(n):
-    numCities = int(input())
-    mySet = set()
-    for _ in range(numCities):
-        x = input()
-        mySet.add(x)
-    print(len(mySet))
